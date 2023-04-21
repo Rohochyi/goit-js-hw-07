0873e1cd4ec1e08a063ad75743aef7167f0b187a
Rohochyi <oxid1109@gmail.com>
2023-04-21 13:53:56 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divRef = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${item.preview}">
+        <img src="${item.preview}" class="gallery__image" data-source="${item.original}" alt="${item.description}">
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+divRef.innerHTML = addGalleryMarkup;
+
+divRef.addEventListener("click", onImegeClick);
+
+// function onImegeClick(e) {
+//   const instance = basicLightbox.create(
+
+// `<img src="${e.target.dataset.source}" widht='800'>`
+  
+  
+//   instance.show();
+// }
