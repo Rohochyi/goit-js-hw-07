d7a292222907bb225995657b51931ddee19b564d
Rohochyi <oxid1109@gmail.com>
2023-04-23 18:05:19 +0300

    fix size

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,9 +6,9 @@ const divRef = document.querySelector(".gallery");
 function createGalleryMarkup(items) {
   return items
     .map(
-      ({ preview, description }) =>
+      ({ preview, original, description }) =>
         `<li class="gallery__item">
-        <a class="gallery__link" href="${preview}">
+        <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
